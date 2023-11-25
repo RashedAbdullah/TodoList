70a4664f6579592e4685b882a766d7535e6b29a9
rashedabdullah <maarashed40@gmail.com>
2023-11-25 16:16:22 +0600

    TodoList

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import TodoList from './Components/Task01/TodoList';
+import PostList from './Components/Task04/PostList';
+import Tasbeeh from './Components/Task05/Tasbeeh';
+const App = ()=> {
+
+
+  return (
+    <div>
+      <TodoList />
+
+      {/* <PostList /> */}
+
+      {/* <Tasbeeh /> */}
+    </div>
+  )
+}
+
+export default App
